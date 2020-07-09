dbbf1dbf1aa8cdfe18a47e3a9cc3c2174f04683b
Brianna Bullock <brianna.bullock16@gmail.com>
2020-07-08 19:41:03 -0500

    added a prompt from inquirer

--- index.js
+++ index.js
@@ -1,3 +1,63 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
+
+function promptQuest() {
+    return inquirer.prompt ([
+        {    
+            type: "input",
+            name: "tile",
+            message: "Project Title:"
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Description:"
+        },
+        {
+            type: "input",
+            name: "instructions",
+            message: "Installation Instructions:"
+        },
+        {
+            type: "input",
+            name: "usage",
+            message: "Usage Information:"
+        },  
+        {
+            type: "input",
+            name: "guidelines",
+            message: "Contribution Guidelines:"
+        },  
+        {
+            type: "input",
+            name: "test",
+            message: "Test Instructions:"
+        },  
+        {
+            type: "list",
+            name: "license",
+            message: "Which license?",
+            choices: [
+                "Apache License 2.0", 
+                "GNU GPLv3", 
+                "MIT", 
+                "ISC License", 
+                "SIL Open Font License 1.1"
+            ]
+        },    
+    ])
+}
+
+promptQuest();
+
+// .then(answers => {
+//     // Use user feedback for... whatever!!
+//   })
+//   .catch(error => {
+//     if(error.isTtyError) {
+//       // Prompt couldn't be rendered in the current environment
+//     } else {
+//       // Something else when wrong
+//     }
+//   });
