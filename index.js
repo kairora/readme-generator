c3c404bdb384c48251bd2949c6db7a894e5b7701
Brianna Bullock <brianna.bullock16@gmail.com>
2020-07-08 22:34:03 -0500

    readme gets created after user response

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const util = require("util");
 
+const asyncFileWriter = util.promisify(fs.writeFile);
+
 function promptQuest() {
     return inquirer.prompt ([
         {    
@@ -9,75 +11,90 @@ function promptQuest() {
             name: "title",
             message: "Project Title:"
         },
-        {
-            type: "input",
-            name: "description",
-            message: "Description:"
-        },
-        {
-            type: "input",
-            name: "instructions",
-            message: "Installation Instructions:"
-        },
-        {
-            type: "input",
-            name: "usage",
-            message: "Usage Information:"
-        },  
-        {
-            type: "input",
-            name: "guidelines",
-            message: "Contribution Guidelines:"
-        },  
-        {
-            type: "input",
-            name: "test",
-            message: "Test Instructions:"
-        },  
-        {
-            type: "list",
-            name: "license",
-            message: "Which license?",
-            choices: [
-                "Apache License 2.0", 
-                "GNU GPLv3", 
-                "MIT", 
-                "ISC License", 
-                "SIL Open Font License 1.1"
-            ]
-        },   
-        {
-            type: "input",
-            name: "github",
-            message: "Github Username:"
-        }, 
-        {
-            type: "input",
-            name: "email",
-            message: "Email Address:"
-        },
-    ]).then(function(result) {
+        // {
+        //     type: "input",
+        //     name: "description",
+        //     message: "Description:"
+        // },
+        // {
+        //     type: "input",
+        //     name: "instructions",
+        //     message: "Installation Instructions:"
+        // },
+        // {
+        //     type: "input",
+        //     name: "usage",
+        //     message: "Usage Information:"
+        // },  
+        // {
+        //     type: "input",
+        //     name: "guidelines",
+        //     message: "Contribution Guidelines:"
+        // },  
+        // {
+        //     type: "input",
+        //     name: "test",
+        //     message: "Test Instructions:"
+        // },  
+        // {
+        //     type: "list",
+        //     name: "license",
+        //     message: "Which license?",
+        //     choices: [
+        //         "Apache License 2.0", 
+        //         "GNU GPLv3", 
+        //         "MIT", 
+        //         "ISC License", 
+        //         "SIL Open Font License 1.1"
+        //     ]
+        // },   
+        // {
+        //     type: "input",
+        //     name: "github",
+        //     message: "Github Username:"
+        // }, 
+        // {
+        //     type: "input",
+        //     name: "email",
+        //     message: "Email Address:"
+        // },
+    ])
 
-        var newReadMe = result.title.toLowerCase().split(' ').join('') + ".md";
-        fs.writeFile(newReadMe, JSON.stringify(result, null, '\t'), function(err) {
-            if (err) {
-            return console.log(err);
-        }
-        console.log("Success!");
 
-        });
-    });
+
+    // .then(function(result) {
+
+    //     var newReadMe = result.title.toLowerCase().split(' ').join('') + ".md";
+    //     fs.writeFile(newReadMe, JSON.stringify(result, null, '\t'), function(err) {
+    //         if (err) {
+    //         return console.log(err);
+    //     }
+    //     console.log("Success!");
+
+    //     });
+    // });
 }
 
-promptQuest();
 
-// .then(answers => {
-//     // Use user feedback for... whatever!!
-//   })
-//   .catch(error => {
-//     if(error.isTtyError) {
-//       // Prompt couldn't be rendered in the current environment
-//     } else {
-//       // Something else when wrong
-//     }
-//   });
+// function for md.
+function createMD(response) {
+    return `
+  # ${response.title}`;
+  }
+
+// promptQuest();
+
+async function startAll() {
+    try {
+      const answers = await promptQuest();
+  
+      const markDown = createMD(answers);
+  
+      await asyncFileWriter("README2.md", markDown);
+  
+      console.log("New readme created!");
+    } catch(err) {
+      console.log(err);
+    }
+  }
+  startAll();
